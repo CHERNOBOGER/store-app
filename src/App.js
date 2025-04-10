53b1455cca9a324ead6eda876796cfd75858cc96
CHERNOBOGER <plata32123@gmail.com>
2025-04-10 17:29:56 +0300

    ds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import NotFoundPage from './components/NotFoundPage';
